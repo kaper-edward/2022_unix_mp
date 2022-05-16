f8bdfc795c1634d67b56b73e2e5f733be8818493
fienestar <fienestar@gmail.com>
2022-05-16 10:24:41 +0900

    Create linkedlist.cpp

---
+++ libs/linkedlist.cpp
@@ -0,0 +1,92 @@
+#include <linkedlist.h>
+
+static Node* _head = NULL;
+static Node* _tail = NULL;
+static Node* _cur_node = NULL;
+
+bool empty()
+{
+    //TODO: IMPL
+    return true;
+}
+
+size_t size()
+{
+    //TODO: IMPL
+    return 0;
+}
+
+void print()
+{
+    //TODO: IMPL
+}
+
+void print_file(FILE* stream)
+{
+    //TODO: IMPL
+}
+
+void clear()
+{
+    //TODO: IMPL
+}
+
+Node* append_left(size_t n, char new_data[n])
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* insert_after(Node* cur_node, Node* new_node)
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* append(size_t n, char new_data[n])
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* delete_node(Node* cur_node)
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* delete(char* data)
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* get_node(size_t index)
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* first()
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* last()
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* next()
+{
+    //TODO: IMPL
+    return NULL;
+}
+
+Node* prev()
+{
+    //TODO: IMPL
+    return NULL;
+}
