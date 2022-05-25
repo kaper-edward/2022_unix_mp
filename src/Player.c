647a1b283d7227cb5f375a98e3f5c43cc54f9f18
jinn-ie <104821183+jinn-ie@users.noreply.github.com>
2022-05-26 02:18:18 +0900

    added format for commands
    형식만 구현. printf는 잘 작동하는 지 확인하기 위해 임시로 넣어놨습니다

--- src/Player.c
+++ src/Player.c
@@ -3,5 +3,53 @@
 #include "textfilewriter.h"
 
 int main() {
-  printf("Helloworld!");
+  char command[50];						//명령어 n번 입력받기
+  scanf("%d", &n);
+  
+  char song[50];	//***동적할당 사용하여 다시 구현
+  char file[1000];
+  int num; 
+  
+  for (int i=0; i<n; i++) {
+  	scanf("%s", command);
+  	
+  	if (!strcmp(command, "add")) {		//명령어별 처리 
+  		scanf("%s", song);
+  		printf("Added %s\n", song);
+    }
+    else if (!strcmp(command, "del")) {
+      scanf("%s", song);
+      printf("Deleted %s\n", song);
+    }
+    else if (!strcmp(command, "list")) {
+      printf("List\n");
+    }
+    else if (!strcmp(command,"next")) {
+      printf("Next\n");
+    }
+    else if (!strcmp(command,"prev")) {
+      printf("Prev\n");
+    }
+    else if (!strcmp(command,"move")) {
+      scanf("%s",num);
+      printf("Moved %d\n");
+    }
+    else if (!strcmp(command,"play")) {
+      printf("Play\n");
+    }
+    else if (!strcmp(command,"clear")) {
+      printf("Clear\n");
+    }
+    else if (!strcmp(command,"quit")) {
+      printf("Quit\n");
+    }
+    else if (!strcmp(command,"load")) {
+      scanf("%s",file);
+      printf("Created playlist\n");
+    }
+    else if (!strcmp(command,"save")) {
+      scanf("%s",file);
+      printf("Saved songs to file\n");
+    }
+  }
 }
